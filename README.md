4ad0e874311b53d3f49dc2be0c63a203234e5a78
Ahmed Serag <ahmed.serag@robustastudio.com>
2020-11-22 21:03:58 +0200

    feat: shift encryption algorithm

---
+++ README.md
@@ -0,0 +1,32 @@
+# BackEnd Python Tasks
+
+## Shift encrypted algorithm
+
+shift every character in the provided string by 3 characters so if word contains letter “A” in the
+encrypted string, “A” will be replaced by “D”.
+
+## Pre-requisites
+
+- [python 3.8 or higher](https://www.python.org)
+
+## Usage
+
+- run the python script `shift-encryption-algorithm.py` with the text you need to run as arguments
+
+```bash
+python shift-encryption-algorithm.py 'Hello world'
+```
+
+- pass multiple arguments for multiple text
+
+```bash
+python shift-encryption-algorithm.py 'Hello world' 'encrypt me'
+```
+
+the script is expected to print the encrypted text for each argument as the following:
+
+```bash
+python shift-encryption-algorithm.py 'Hello world' 'encrypt me'
+Hello world -> Khoor zruog
+encrypt me -> hqfu|sw ph
+```
---
+++ shift-encryption-algorithm.py
@@ -0,0 +1,23 @@
+import sys
+
+def shiftEncryption(text: str) -> str:
+    '''encrypt text using shiftEncrypting algorithm with base 3'''
+    encryptedWords = []
+    words = text.split(' ')
+    for word in words:
+        encrypted = ''
+        for c in word:
+            asciiValue = ord(c) # ascii value of the char
+            encrypted += chr(asciiValue + 3) # shift it by 3 and return it
+        encryptedWords.append(encrypted)
+    return ' '.join(encryptedWords)
+
+if __name__ == "__main__":
+    '''ask user for a text to encrypt in case of main'''
+    args = sys.argv[1:]
+    encryptedArgs = [] # array of encrypted values of the provided args
+    for arg in args:
+        encryptedValue = shiftEncryption(arg)
+        encryptedArgs.append(encryptedValue)
+    for i in range(len(encryptedArgs)):
+        print(args[i], "->",encryptedArgs[i])
